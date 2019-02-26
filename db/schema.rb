6b7f5021fe9091fa98151ee3af9fed3d88423828
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-02-26 18:05:20 +0100

    Added Picture Url to Flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_162133) do
+ActiveRecord::Schema.define(version: 2019_02_26_170354) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_162133) do
     t.integer "number_of_guests"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
   end
 
 end
